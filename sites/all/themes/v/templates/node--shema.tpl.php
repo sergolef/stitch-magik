91ae55edaf32a08980ef1c2107567a98fad0807c
sergolef <sergolef@gmail.com>
2012-08-23 02:39:07 +0300

    0.1
    drupal development site

---
+++ sites/all/themes/v/templates/node--shema.tpl.php
@@ -0,0 +1,8 @@
+<div class="scheme"> 
+				<?php print render($content['body']) ?>
+				<?php print render($content['field_preview']) ?>
+				<?php print render($content['field_size']) ?>
+				<?php print render($content['field_category']) ?>
+				<?php print render($content['links']) ?>
+			</div>
+			
