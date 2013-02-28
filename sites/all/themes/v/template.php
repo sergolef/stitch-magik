0a9bf84808b66af0a9ba45d1266af4d0f52869ba
sergolef <sergolef@gmail.com>
2013-02-28 07:15:38 +0200

    added google font and modified search form

--- sites/all/themes/v/template.php
+++ sites/all/themes/v/template.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @file
  * Contains the theme's functions to manipulate Drupal's default markup.
@@ -100,8 +101,6 @@
  *   please visit the Theme Developer's Guide on Drupal.org:
  *   http://drupal.org/node/223440 and http://drupal.org/node/1089656
  */
-
-
 /**
  * Override or insert variables into the maintenance page template.
  *
@@ -111,14 +110,14 @@
  *   The name of the template being rendered ("maintenance_page" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_maintenance_page(&$variables, $hook) {
+  function STARTERKIT_preprocess_maintenance_page(&$variables, $hook) {
   // When a variable is manipulated or added in preprocess_html or
   // preprocess_page, that same work is probably needed for the maintenance page
   // as well, so we can just re-use those functions to do that work here.
   STARTERKIT_preprocess_html($variables, $hook);
   STARTERKIT_preprocess_page($variables, $hook);
-}
-// */
+  }
+  // */
 
 /**
  * Override or insert variables into the html templates.
@@ -129,14 +128,14 @@ function STARTERKIT_preprocess_maintenance_page(&$variables, $hook) {
  *   The name of the template being rendered ("html" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_html(&$variables, $hook) {
+  function STARTERKIT_preprocess_html(&$variables, $hook) {
   $variables['sample_variable'] = t('Lorem ipsum.');
 
   // The body tag's classes are controlled by the $classes_array variable. To
   // remove a class from $classes_array, use array_diff().
   //$variables['classes_array'] = array_diff($variables['classes_array'], array('class-to-remove'));
-}
-// */
+  }
+  // */
 
 /**
  * Override or insert variables into the page templates.
@@ -147,10 +146,10 @@ function STARTERKIT_preprocess_html(&$variables, $hook) {
  *   The name of the template being rendered ("page" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_page(&$variables, $hook) {
+  function STARTERKIT_preprocess_page(&$variables, $hook) {
   $variables['sample_variable'] = t('Lorem ipsum.');
-}
-// */
+  }
+  // */
 
 /**
  * Override or insert variables into the node templates.
@@ -161,17 +160,17 @@ function STARTERKIT_preprocess_page(&$variables, $hook) {
  *   The name of the template being rendered ("node" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_node(&$variables, $hook) {
+  function STARTERKIT_preprocess_node(&$variables, $hook) {
   $variables['sample_variable'] = t('Lorem ipsum.');
 
   // Optionally, run node-type-specific preprocess functions, like
   // STARTERKIT_preprocess_node_page() or STARTERKIT_preprocess_node_story().
   $function = __FUNCTION__ . '_' . $variables['node']->type;
   if (function_exists($function)) {
-    $function($variables, $hook);
+  $function($variables, $hook);
   }
-}
-// */
+  }
+  // */
 
 /**
  * Override or insert variables into the comment templates.
@@ -182,10 +181,10 @@ function STARTERKIT_preprocess_node(&$variables, $hook) {
  *   The name of the template being rendered ("comment" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_comment(&$variables, $hook) {
+  function STARTERKIT_preprocess_comment(&$variables, $hook) {
   $variables['sample_variable'] = t('Lorem ipsum.');
-}
-// */
+  }
+  // */
 
 /**
  * Override or insert variables into the region templates.
@@ -196,13 +195,13 @@ function STARTERKIT_preprocess_comment(&$variables, $hook) {
  *   The name of the template being rendered ("region" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_region(&$variables, $hook) {
+  function STARTERKIT_preprocess_region(&$variables, $hook) {
   // Don't use Zen's region--sidebar.tpl.php template for sidebars.
   //if (strpos($variables['region'], 'sidebar_') === 0) {
   //  $variables['theme_hook_suggestions'] = array_diff($variables['theme_hook_suggestions'], array('region__sidebar'));
   //}
-}
-// */
+  }
+  // */
 
 /**
  * Override or insert variables into the block templates.
@@ -213,7 +212,7 @@ function STARTERKIT_preprocess_region(&$variables, $hook) {
  *   The name of the template being rendered ("block" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_block(&$variables, $hook) {
+  function STARTERKIT_preprocess_block(&$variables, $hook) {
   // Add a count to all the blocks in the region.
   // $variables['classes_array'][] = 'count-' . $variables['block_id'];
 
@@ -222,141 +221,148 @@ function STARTERKIT_preprocess_block(&$variables, $hook) {
   //if ($variables['block_html_id'] == 'block-system-main') {
   //  $variables['theme_hook_suggestions'] = array_diff($variables['theme_hook_suggestions'], array('block__no_wrapper'));
   //}
-}
+  }
 
-// */
+  // */
 /*
-function v_theme_links__menu_modern_tchnic($variables) 
-{
-   $output = '';
-   foreach ($variables['links'] as $link) 
-   {
-      $output .=  l('<div>'.$link['title'].'</div>', $link['href'],array('html' => True));
-   }
-   return $output;
-}*/
+  function v_theme_links__menu_modern_tchnic($variables)
+  {
+  $output = '';
+  foreach ($variables['links'] as $link)
+  {
+  $output .=  l('<div>'.$link['title'].'</div>', $link['href'],array('html' => True));
+  }
+  return $output;
+  } */
 
-/** 
-*rebuild medern technic menu
-*/
+/**
+ * rebuild medern technic menu
+ */
 function v_menu_link__menu_mmenu($variables) {
-  $element = $variables['element'];
-    $output = l(''.$element['#title'].'', $element['#href'], $options = array('html' => TRUE));
+    $element = $variables['element'];
+    $output = l('' . $element['#title'] . '', $element['#href'], $options = array('html' => TRUE));
     return '<li class="row_item" ' . drupal_attributes($element['#attributes']) . '><span class="arrow"></span>' . $output . "</li>\n";
 }
+
 //
 //rebuild rare technic 
 function v_menu_link__menu_raremenu($variables) {
-  $element = $variables['element'];
-    $output = l(''.$element['#title'].'', $element['#href'], $options = array('html' => TRUE));
+    $element = $variables['element'];
+    $output = l('' . $element['#title'] . '', $element['#href'], $options = array('html' => TRUE));
     return '<li class="row_item" ' . drupal_attributes($element['#attributes']) . '><span class="arrow"></span>' . $output . "</li>\n";
 }
+
 function v_pager__scheme_categories_block($variables) {
-	dpm('ssfsdf');
-	$tags = $variables['tags'];
-	$element = $variables['element'];
-	$parameters = $variables['parameters'];
-	$quantity = $variables['quantity'];
-	global $pager_page_array, $pager_total;
+    dpm('ssfsdf');
+    $tags = $variables['tags'];
+    $element = $variables['element'];
+    $parameters = $variables['parameters'];
+    $quantity = $variables['quantity'];
+    global $pager_page_array, $pager_total;
 
-	// Calculate various markers within this pager piece:
-	// Middle is used to "center" pages around the current page.
-	$pager_middle = ceil($quantity / 2);
-	// current is the page we are currently paged to
-	$pager_current = $pager_page_array[$element] + 1;
-	// first is the first page listed by this pager piece (re quantity)
-	$pager_first = $pager_current - $pager_middle + 1;
-	// last is the last page listed by this pager piece (re quantity)
-	$pager_last = $pager_current + $quantity - $pager_middle;
-	// max is the maximum page number
-	$pager_max = $pager_total[$element];
-	// End of marker calculations.
+    // Calculate various markers within this pager piece:
+    // Middle is used to "center" pages around the current page.
+    $pager_middle = ceil($quantity / 2);
+    // current is the page we are currently paged to
+    $pager_current = $pager_page_array[$element] + 1;
+    // first is the first page listed by this pager piece (re quantity)
+    $pager_first = $pager_current - $pager_middle + 1;
+    // last is the last page listed by this pager piece (re quantity)
+    $pager_last = $pager_current + $quantity - $pager_middle;
+    // max is the maximum page number
+    $pager_max = $pager_total[$element];
+    // End of marker calculations.
+    // Prepare for generation loop.
+    $i = $pager_first;
+    if ($pager_last > $pager_max) {
+        // Adjust "center" if at end of query.
+        $i = $i + ($pager_max - $pager_last);
+        $pager_last = $pager_max;
+    }
+    if ($i <= 0) {
+        // Adjust "center" if at start of query.
+        $pager_last = $pager_last + (1 - $i);
+        $i = 1;
+    }
+    // End of generation loop preparation.
 
-	// Prepare for generation loop.
-	$i = $pager_first;
-	if ($pager_last > $pager_max) {
-		// Adjust "center" if at end of query.
-		$i = $i + ($pager_max - $pager_last);
-		$pager_last = $pager_max;
-	}
-	if ($i <= 0) {
-		// Adjust "center" if at start of query.
-		$pager_last = $pager_last + (1 - $i);
-		$i = 1;
-	}
-	// End of generation loop preparation.
+    $li_first = theme('pager_first', array('text' => (isset($tags[0]) ? $tags[0] : t('« first')), 'element' => $element, 'parameters' => $parameters));
+    $li_previous = theme('pager_previous', array('text' => (isset($tags[1]) ? $tags[1] : t('‹ previous')), 'element' => $element, 'interval' => 1, 'parameters' => $parameters));
+    $li_next = theme('pager_next', array('text' => (isset($tags[3]) ? $tags[3] : t('next ›')), 'element' => $element, 'interval' => 1, 'parameters' => $parameters));
+    $li_last = theme('pager_last', array('text' => (isset($tags[4]) ? $tags[4] : t('last »')), 'element' => $element, 'parameters' => $parameters));
 
-	$li_first = theme('pager_first', array('text' => (isset($tags[0]) ? $tags[0] : t('« first')), 'element' => $element, 'parameters' => $parameters));
-	$li_previous = theme('pager_previous', array('text' => (isset($tags[1]) ? $tags[1] : t('‹ previous')), 'element' => $element, 'interval' => 1, 'parameters' => $parameters));
-	$li_next = theme('pager_next', array('text' => (isset($tags[3]) ? $tags[3] : t('next ›')), 'element' => $element, 'interval' => 1, 'parameters' => $parameters));
-	$li_last = theme('pager_last', array('text' => (isset($tags[4]) ? $tags[4] : t('last »')), 'element' => $element, 'parameters' => $parameters));
+    if ($pager_total[$element] > 1) {
+        if ($li_first) {
+            $items[] = array(
+              'class' => array('pager-first'),
+              'data' => $li_first,
+            );
+        }
+        if ($li_previous) {
+            $items[] = array(
+              'class' => array('pager-previous'),
+              'data' => $li_previous,
+            );
+        }
 
-	if ($pager_total[$element] > 1) {
-		if ($li_first) {
-			$items[] = array(
-					'class' => array('pager-first'),
-					'data' => $li_first,
-			);
-		}
-		if ($li_previous) {
-			$items[] = array(
-					'class' => array('pager-previous'),
-					'data' => $li_previous,
-			);
-		}
-
-		// When there is more than one page, create the pager list.
-		if ($i != $pager_max) {
-			if ($i > 1) {
-				$items[] = array(
-						'class' => array('pager-ellipsis'),
-						'data' => '…',
-				);
-			}
-			// Now generate the actual pager piece.
-			for (; $i <= $pager_last && $i <= $pager_max; $i++) {
-				if ($i < $pager_current) {
-					$items[] = array(
-							'class' => array('pager-item'),
-							'data' => theme('pager_previous', array('text' => $i, 'element' => $element, 'interval' => ($pager_current - $i), 'parameters' => $parameters)),
-					);
-				}
-				if ($i == $pager_current) {
-					$items[] = array(
-							'class' => array('pager-current'),
-							'data' => $i,
-					);
-				}
-				if ($i > $pager_current) {
-					$items[] = array(
-							'class' => array('pager-item'),
-							'data' => theme('pager_next', array('text' => $i, 'element' => $element, 'interval' => ($i - $pager_current), 'parameters' => $parameters)),
-					);
-				}
-			}
-			if ($i < $pager_max) {
-				$items[] = array(
-						'class' => array('pager-ellipsis'),
-						'data' => '…',
-				);
-			}
-		}
-		// End generation.
-		if ($li_next) {
-			$items[] = array(
-					'class' => array('pager-next'),
-					'data' => $li_next,
-			);
-		}
-		if ($li_last) {
-			$items[] = array(
-					'class' => array('pager-last'),
-					'data' => $li_last,
-			);
-		}
-		return '<h2 class="element-invisible">' . t('Pages') . '</h2>' . theme('item_list', array(
-				'items' => $items,
-				'attributes' => array('class' => array('pager')),
-		));
-	}
+        // When there is more than one page, create the pager list.
+        if ($i != $pager_max) {
+            if ($i > 1) {
+                $items[] = array(
+                  'class' => array('pager-ellipsis'),
+                  'data' => '…',
+                );
+            }
+            // Now generate the actual pager piece.
+            for (; $i <= $pager_last && $i <= $pager_max; $i++) {
+                if ($i < $pager_current) {
+                    $items[] = array(
+                      'class' => array('pager-item'),
+                      'data' => theme('pager_previous', array('text' => $i, 'element' => $element, 'interval' => ($pager_current - $i), 'parameters' => $parameters)),
+                    );
+                }
+                if ($i == $pager_current) {
+                    $items[] = array(
+                      'class' => array('pager-current'),
+                      'data' => $i,
+                    );
+                }
+                if ($i > $pager_current) {
+                    $items[] = array(
+                      'class' => array('pager-item'),
+                      'data' => theme('pager_next', array('text' => $i, 'element' => $element, 'interval' => ($i - $pager_current), 'parameters' => $parameters)),
+                    );
+                }
+            }
+            if ($i < $pager_max) {
+                $items[] = array(
+                  'class' => array('pager-ellipsis'),
+                  'data' => '…',
+                );
+            }
+        }
+        // End generation.
+        if ($li_next) {
+            $items[] = array(
+              'class' => array('pager-next'),
+              'data' => $li_next,
+            );
+        }
+        if ($li_last) {
+            $items[] = array(
+              'class' => array('pager-last'),
+              'data' => $li_last,
+            );
+        }
+        return '<h2 class="element-invisible">' . t('Pages') . '</h2>' . theme('item_list', array(
+              'items' => $items,
+              'attributes' => array('class' => array('pager')),
+            ));
+    }
 }
+
+function V_form_alter(&$form, &$form_state, $form_id) {
+  if ($form_id == 'search_block_form') {
+    $form['search_block_form']['#attributes']['placeholder'] = t(' искать...');
+  }
+} 
--- sites/all/themes/v/templates/page.tpl.php
+++ sites/all/themes/v/templates/page.tpl.php
@@ -1,96 +1,96 @@
 
-		<div id="body1">
-			<div class="body1_top"> </div>
-			<div class="body1_center"> </div>
-			<div class="body1_bottom"> </div>
-		</div>
-		<div id='header'>
-			<div class="header_top">
-			<h1 class="sate_name">
-                <?php print render($page['header']); ?>
-                Stitch Magic
-            </h1>
-            </div>
-            <div class="search_form">
-				<div class="internal_box">
-					<p class="search_name"></p>
-				</div>
-				<div class="internal_box">
-					<p class="input_field"><?php print render($page['search']); ?></p>
-					
-				</div>
-			</div>
-            
-                <div id="search"></div>
-
-            
-
-            <div id=menu><?php print render($page['h_menu']); ?></div>
-
-
-
-			<div class="central_picture" style=""><?php print render($page['slider']); ?></div>
-		</div>
-		
-		<div id="body2">
-
-		<div id='left_bar'>
-            <?php print render($page['sidebar_left']); ?>
-		</div>
-
-            <div id="content" class="column" role="main">
-                <?php print render($page['highlighted']); ?>
-                <?php print $breadcrumb; ?>
-                <a id="main-content"></a>
-                <?php print render($title_prefix); ?>
-                <?php if ($title): ?>
-                <h1 class="title" id="page-title"><?php print $title; ?></h1>
-                <?php endif; ?>
-                <?php print render($title_suffix); ?>
-                <?php print $messages; ?>
-                <?php print render($tabs); ?>
-                <?php print render($page['help']); ?>
-                <?php if ($action_links): ?>
-                <ul class="action-links"><?php print render($action_links); ?></ul>
-                <?php endif; ?>
-                <?php print render($page['content']); ?>
-                <?php print $feed_icons; ?>
-            </div><!-- /#content -->
+<div id="body1">
+    <div class="body1_top"> </div>
+    <div class="body1_center"> </div>
+    <div class="body1_bottom"> </div>
+</div>
+<div id='header'>
+    <div class="header_top">
+        <h1 class="sate_name">
+            <?php print render($page['header']); ?>
+            Stitch Magic
+        </h1>
+    </div>
+    <div class="search_form">
+        <div class="internal_box">
+            <p class="search_name"></p>
         </div>
+        <div class="internal_box">
+            <p class="input_field"><?php print render($page['search']); ?></p>
 
-                <div id="triptych-wrapper"><div id="triptych" class="clearfix">
-                    <div class="footer_coloumn">1<?php print render($page['triptych_first']); ?></div>
-                    <div class="separator"></div>
-                    <div class="footer_coloumn">2<?php print render($page['triptych_middle']); ?></div>
-                    <div class="separator"></div>
-                    <div class="footer_coloumn">3<?php print render($page['triptych_last']); ?></div>
-                </div></div> <!-- /#triptych, /#triptych-wrapper -->
+        </div>
+    </div>
+
+    <div id="search"></div>
 
 
-        <div class="clear_fix">&nbsp;</div>
-        <div id='footer'>
-            <div class="body1_top"> </div>
-            <div id="top_footer">
-                <div id="coloums">
 
-                    <div class="footer_coloumn">
-                        <?php print render($page['prefooter1']); ?>
-                    </div>
-                    <div class="footer_coloumn">
-                        <?php print render($page['prefooter2']); ?>
-                     </div>
-                    <div class="footer_coloumn">
-                        <?php print render($page['prefooter3']); ?>
-                    </div>
-                </div>
-                <div class="footer_line"> </div>
-                <div id="sign_up_form">
+    <div id=menu><?php print render($page['h_menu']); ?></div>
 
-                </div>
+
+
+    <div class="central_picture" style=""><?php print render($page['slider']); ?></div>
+</div>
+
+<div id="body2">
+
+    <div id='left_bar'>
+        <?php print render($page['sidebar_left']); ?>
+    </div>
+
+    <div id="content" class="column" role="main">
+        <?php print render($page['highlighted']); ?>
+        <?php print $breadcrumb; ?>
+        <a id="main-content"></a>
+        <?php print render($title_prefix); ?>
+        <?php if ($title): ?>
+            <h1 class="title" id="page-title"><?php print $title; ?></h1>
+        <?php endif; ?>
+        <?php print render($title_suffix); ?>
+        <?php print $messages; ?>
+        <?php print render($tabs); ?>
+        <?php print render($page['help']); ?>
+        <?php if ($action_links): ?>
+            <ul class="action-links"><?php print render($action_links); ?></ul>
+        <?php endif; ?>
+        <?php print render($page['content']); ?>
+        <?php print $feed_icons; ?>
+    </div><!-- /#content -->
+</div>
+
+<div id="triptych-wrapper"><div id="triptych" class="clearfix">
+        <div class="footer_coloumn">1<?php print render($page['triptych_first']); ?></div>
+        <div class="separator"></div>
+        <div class="footer_coloumn">2<?php print render($page['triptych_middle']); ?></div>
+        <div class="separator"></div>
+        <div class="footer_coloumn">3<?php print render($page['triptych_last']); ?></div>
+    </div></div> <!-- /#triptych, /#triptych-wrapper -->
+
+
+<div class="clear_fix">&nbsp;</div>
+<div id='footer'>
+    <div class="body1_top"> </div>
+    <div id="top_footer">
+        <div id="coloums">
+
+            <div class="footer_coloumn">
+                <?php print render($page['prefooter1']); ?>
             </div>
-            <div id="bottom_footer">
-                <p>Copyright by Natashenka.Mir 2012</p>
-                <?php print render($page['footer']); ?>
+            <div class="footer_coloumn">
+                <?php print render($page['prefooter2']); ?>
+            </div>
+            <div class="footer_coloumn">
+                <?php print render($page['prefooter3']); ?>
             </div>
         </div>
+        <div class="footer_line"> </div>
+        <div id="sign_up_form">
+
+        </div>
+    </div>
+    <div id="bottom_footer">
+        <p>Copyright by Natashenka.Mir 2012</p>
+        <?php print render($page['footer']); ?>
+    </div>
+</div>
 
